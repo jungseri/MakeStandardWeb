5055d0e59ca43735fd2a842d74c8129a06cee3ec
jjest2222 <jjest2222@gmail.com>
2020-06-02 21:06:32 +0900

    탭메뉴가안되긴하지만,일단탭메뉴까지

--- js/jquery01.js
+++ js/jquery01.js
@@ -1,9 +1,3 @@
 $(document).ready(function () {
-    
-$(".tit .btn").click(function(e){
-    // e.preventDefault();
-    $("#cont_nav").css("display","block");
-    //#cont_nav{display:block}
-})    
 
 });
