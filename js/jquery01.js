b7f296c54d2c396ff414b26bde4914f38f6e048c
jjest2222 <jjest2222@gmail.com>
2020-06-02 14:44:20 +0900

    2020.06.01_html,css끝

---
+++ js/jquery01.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    
+$(".tit .btn").click(function(e){
+    // e.preventDefault();
+    $("#cont_nav").css("display","block");
+    //#cont_nav{display:block}
+})    
+
+});
